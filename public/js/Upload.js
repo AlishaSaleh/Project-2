27a535ca758241f582ef2ce538a219d25fb15b68
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-04-09 15:18:50 +0100

    removed seed from my json package as didn't want to re-seed

--- public/js/Upload.js
+++ public/js/Upload.js
@@ -1,7 +1,11 @@
 //Upload product image 
 //Main URL to cloudinary
-const url = "https://api.cloudinary.com/v1_1/blushing-cow-limited/image/upload/";
+const url = "https://api.cloudinary.com/v1_1/blushing-cow-limited/image/upload";
+
 const form = document.querySelector("form");
+
+//testing with a url
+imageurl = "'../../images/spencer-davis-unsplash.jpg'";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,6 +15,7 @@ form.addEventListener("submit", (e) => {
   if(files.length)
   {
   for (let i = 0; i < files.length; i++) {
+    console.log("in upload");
     let file = files[i];
     formData.append("file", file);
     formData.append("upload_preset", "ml_default");
@@ -22,11 +27,12 @@ form.addEventListener("submit", (e) => {
       .then((response) => {
         return response.text();       
       })
+
       .then((data) => {
         //Append to restaurant page 
         var data = JSON.parse(data); //Parse data from the uploaded image to JSON
         var imageurl = data.url; //Get the URL of the uploaded image 
-        var div = document.querySelector("#product_image");
+        var div = document.querySelector("#restaurant_image");
         var image = document.createElement("img");
         image.setAttribute("id", "restaurant-image");
         image.width ="150";
@@ -40,5 +46,6 @@ else
 // If no image is selected, alert user
 {
 window.alert("Please select file to upload");
+console.log("no image selected");
 }
 });
--- seeds/restaurant-seeds.js
+++ seeds/restaurant-seeds.js
@@ -5,11 +5,13 @@ const restaurantData =
 [
   {
     "name": "Carluccio's",
-    "location": "Bull Ring"
+    "location": "Bull Ring", 
+    "img_url": "https://github.com/Loosekonnection/believe/blob/main/img/Tomatoes.JPG"
   },
   {
     "name": "Tapas Revolution",
-    "location": "Stephenson Street, Birmingham"
+    "location": "Stephenson Street, Birmingham",
+    "img_url": "https://res.cloudinary.com/blushing-cow-limited/image/upload/v1617825459/samples/food/spices.jpg"
  },{
     "name": "Turtle Bay",
     "location": "John Bright Street, Birmingham"
