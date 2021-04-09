30e8b7789f6cc3418b6059f6adee2994ee10f30d
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-04-09 15:14:25 +0100

    added image field so can be added in restauarant model

--- public/js/addrestaurant.js
+++ public/js/addrestaurant.js
@@ -3,12 +3,21 @@ async function newFormHandler(event) {
 
   const name = document.querySelector('input[name="restaurant-name"]').value.trim();
   const location = document.querySelector('input[name="restaurant-location"]').value.trim();
+const imageEl = document.getElementbyId("restaurant-image");
+
+if(document.body.contains(imageEl)) //If an image is uploaded, use the url as the image source 
+    {
+      img_url = imageEl.src;
+    }
+
+img_url = '../../images/spencer-david-unsplash.jpg';
 
   const response = await fetch(`/api/restaurants`, {
     method: 'POST',
     body: JSON.stringify({
       name,
-      location
+      location, 
+      img_url
     }),
     headers: {
       'Content-Type': 'application/json'
@@ -18,6 +27,7 @@ async function newFormHandler(event) {
 
   if (response.ok) {
     // send to page that shows all restaurants
+    window.alert("New Restaurant added");
     document.location.replace('/dashboard');
   } else {
     alert(response.statusText);
